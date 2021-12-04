2f28b2fd0e4c73fd8a43dccf355b043e2e754d5f
Yusaku Nishi <jrsaruo@gmail.com>
2021-12-05 02:22:17 +0900

    add: LayoutItem

---
+++ Sources/ConciseLayout/LayoutItem.swift
@@ -0,0 +1,35 @@
+//
+//  LayoutItem.swift
+//  
+//
+//  Created by Yusaku Nishi on 2021/12/04.
+//
+
+import UIKit
+
+public struct LayoutItem {
+    let base: LayoutTarget
+    
+    init(_ target: LayoutTarget) {
+        base = target
+    }
+    
+    // MARK: - XAxes
+    
+    public var leading: LayoutRect.XAxis { .init(target: base, keyPath: \.leadingAnchor) }
+    public var trailing: LayoutRect.XAxis { .init(target: base, keyPath: \.trailingAnchor) }
+    public var left: LayoutRect.XAxis { .init(target: base, keyPath: \.leftAnchor) }
+    public var right: LayoutRect.XAxis { .init(target: base, keyPath: \.rightAnchor) }
+    public var centerX: LayoutRect.XAxis { .init(target: base, keyPath: \.centerXAnchor) }
+    
+    // MARK: - YAxes
+    
+    public var top: LayoutRect.YAxis { .init(target: base, keyPath: \.topAnchor) }
+    public var bottom: LayoutRect.YAxis { .init(target: base, keyPath: \.bottomAnchor) }
+    public var centerY: LayoutRect.YAxis { .init(target: base, keyPath: \.centerYAnchor) }
+    
+    // MARK: - Dimensions
+    
+    public var width: LayoutRect.Dimension { .init(target: base, keyPath: \.widthAnchor) }
+    public var height: LayoutRect.Dimension { .init(target: base, keyPath: \.heightAnchor) }
+}
