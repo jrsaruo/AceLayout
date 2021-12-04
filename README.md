f999b53524bb1f2284e732cb4ead5b45a7243766
Yusaku Nishi <jrsaruo@gmail.com>
2021-12-04 15:45:01 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ConciseLayout
+
+A description of this package.
---
+++ Sources/ConciseLayout/ConciseLayout.swift
@@ -0,0 +1,6 @@
+public struct ConciseLayout {
+    public private(set) var text = "Hello, World!"
+    
+    public init() {
+    }
+}
