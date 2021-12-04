58f18e4a47a379a51b7aa5ce282926406f7c4d83
Yusaku Nishi <jrsaruo@gmail.com>
2021-12-05 02:48:07 +0900

    add: LayoutTarget.autoLayout(activates:, builder:)

--- Sources/ConciseLayout/LayoutTarget.swift
+++ Sources/ConciseLayout/LayoutTarget.swift
@@ -20,7 +20,42 @@ public protocol LayoutTarget {
     
     var widthAnchor: NSLayoutDimension { get }
     var heightAnchor: NSLayoutDimension { get }
+    
+    func autoLayout(activates: Bool,
+                    @LayoutConstraintsBuilder builder: (LayoutItem) -> [NSLayoutConstraint]) -> [NSLayoutConstraint]
 }
 
-extension UIView: LayoutTarget {}
+extension LayoutTarget {
+    
+    /// Default implement of `autoLayout(activates:, builder:)`.
+    /// - Parameters:
+    ///   - activates: Whether to immediately activate built constraints.
+    ///   - builder: AutoLayout constraints builder.
+    /// - Returns: Built constraints.
+    fileprivate func _autoLayout(activates: Bool,
+                                 @LayoutConstraintsBuilder builder: (LayoutItem) -> [NSLayoutConstraint]) -> [NSLayoutConstraint] {
+        let constraints = builder(LayoutItem(self))
+        if activates {
+            NSLayoutConstraint.activate(constraints)
+        }
+        return constraints
+    }
+    
+    @discardableResult
+    public func autoLayout(activates: Bool = true,
+                           @LayoutConstraintsBuilder builder: (LayoutItem) -> [NSLayoutConstraint]) -> [NSLayoutConstraint] {
+        _autoLayout(activates: activates, builder: builder)
+    }
+}
+
+extension UIView: LayoutTarget {
+    
+    @discardableResult
+    public func autoLayout(activates: Bool = true,
+                           @LayoutConstraintsBuilder builder: (LayoutItem) -> [NSLayoutConstraint]) -> [NSLayoutConstraint] {
+        translatesAutoresizingMaskIntoConstraints = false
+        return _autoLayout(activates: activates, builder: builder)
+    }
+}
+
 extension UILayoutGuide: LayoutTarget {}
