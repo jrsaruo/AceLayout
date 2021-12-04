7e5f359d96e3ba170bd17c5c0bc0f409e2bc41e5
Yusaku Nishi <jrsaruo@gmail.com>
2021-12-05 02:15:16 +0900

    add: LayoutAnchor.equal(to:)

--- Sources/ConciseLayout/LayoutAnchors.swift
+++ Sources/ConciseLayout/LayoutAnchors.swift
@@ -13,4 +13,14 @@ public protocol LayoutAnchor {
     
     var target: LayoutTarget { get }
     var keyPath: KeyPath<LayoutTarget, BaseLayoutAnchor> { get }
+    
+    func equal<Target>(to another: Target) -> NSLayoutConstraint where Target: LayoutTarget
+}
+
+extension LayoutAnchor {
+    var anchor: BaseLayoutAnchor { target[keyPath: keyPath] }
+    
+    public func equal<Target>(to another: Target) -> NSLayoutConstraint where Target: LayoutTarget {
+        anchor.constraint(equalTo: another[keyPath: keyPath])
+    }
 }
