a5a066b48891b4cd3c82701e95e31b806d217372
Yusaku Nishi <jrsaruo@gmail.com>
2021-12-16 23:59:32 +0900

    add: shims for UIKit and AppKit

---
+++ Sources/ConciseLayout/Shims.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yusaku Nishi on 2021/12/16.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+public typealias View = UIView
+public typealias LayoutGuide = UILayoutGuide
+public typealias LayoutPriority = UILayoutPriority
+public typealias EdgeInsets = UIEdgeInsets
+
+#elseif canImport(AppKit)
+
+import AppKit
+
+public typealias View = NSView
+public typealias LayoutGuide = NSLayoutGuide
+public typealias LayoutPriority = NSLayoutConstraint.Priority
+public typealias EdgeInsets = NSEdgeInsets
+
+extension EdgeInsets {
+    public static var zero: Self { .init() }
+}
+
+#endif
