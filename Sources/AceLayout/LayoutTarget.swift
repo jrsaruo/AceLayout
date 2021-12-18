48f07089cac07b97c77f117f5598e763e51ec527
Yusaku Nishi <jrsaruo@gmail.com>
2021-12-19 04:18:57 +0900

    docs: LayoutTarget document comments

--- Sources/AceLayout/LayoutTarget.swift
+++ Sources/AceLayout/LayoutTarget.swift
@@ -11,19 +11,24 @@ import UIKit
 import AppKit
 #endif
 
+/// A type that represents layout target such as `UIView`, `UILayoutGuide`, `NSView` or `NSLayoutGuide`.
 public protocol LayoutTarget {
+    /// A type that represents layout target such as `UIView`, `UILayoutGuide`, `NSView` or `NSLayoutGuide`.
+    ///
+    /// This is `Self` in common usage.
     associatedtype LayoutBase
+    
     func autoLayout(activates: Bool,
                     @LayoutConstraintsBuilder builder: (LayoutItem<LayoutBase>) -> [NSLayoutConstraint]) -> [NSLayoutConstraint]
 }
 
 extension LayoutTarget where LayoutBase == Self {
     
-    /// Default implement of `autoLayout(activates:, builder:)`.
+    /// The default implementation of `autoLayout(activates:, builder:)`.
     /// - Parameters:
-    ///   - activates: Whether to immediately activate built constraints.
-    ///   - builder: AutoLayout constraints builder.
-    /// - Returns: Built constraints.
+    ///   - activates: Whether to immediately activate constraints after building.
+    ///   - builder: A layout constraints builder that creates constraints on the specified item.
+    /// - Returns: An array of built `NSLayoutConstraint` objects.
     fileprivate func _autoLayout(activates: Bool,
                                  @LayoutConstraintsBuilder builder: (LayoutItem<LayoutBase>) -> [NSLayoutConstraint]) -> [NSLayoutConstraint] {
         let constraints = builder(LayoutItem(base: self))
@@ -33,6 +38,21 @@ extension LayoutTarget where LayoutBase == Self {
         return constraints
     }
     
+    /// Creates layout constraints.
+    ///
+    /// The `LayoutItem` instance passed to the `builder` has various layout anchors, and you can use them to create layout constraints.
+    ///
+    /// ```
+    /// view.autoLayout { item in
+    ///     item.center.equalToSuperview()
+    ///     item.size.equal(to: referenceView)
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - activates: Whether to immediately activate constraints after building. The default value is `true`.
+    ///   - builder: A layout constraints builder that creates constraints on the specified item.
+    /// - Returns: An array of built `NSLayoutConstraint` objects.
     @discardableResult
     public func autoLayout(activates: Bool = true,
                            @LayoutConstraintsBuilder builder: (LayoutItem<LayoutBase>) -> [NSLayoutConstraint]) -> [NSLayoutConstraint] {
@@ -42,6 +62,22 @@ extension LayoutTarget where LayoutBase == Self {
 
 extension View: LayoutTarget {
     
+    /// Creates layout constraints.
+    ///
+    /// The `LayoutItem` instance passed to the `builder` has various layout anchors, and you can use them to create layout constraints.
+    ///
+    /// ```
+    /// view.autoLayout { item in
+    ///     item.center.equalToSuperview()
+    ///     item.size.equal(to: referenceView)
+    /// }
+    /// ```
+    ///
+    /// - Note: This method automatically sets `translatesAutoresizingMaskIntoConstraints` to `false`.
+    /// - Parameters:
+    ///   - activates: Whether to immediately activate constraints after building. The default value is `true`.
+    ///   - builder: A layout constraints builder that creates constraints on the specified item.
+    /// - Returns: An array of built `NSLayoutConstraint` objects.
     @discardableResult
     public func autoLayout(activates: Bool = true,
                            @LayoutConstraintsBuilder builder: (LayoutItem<View>) -> [NSLayoutConstraint]) -> [NSLayoutConstraint] {
