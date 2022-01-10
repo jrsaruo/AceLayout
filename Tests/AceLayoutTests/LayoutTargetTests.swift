5e08bdfc9a34d462bbf6a9708a995c38aba28350
Yusaku Nishi <jrsaruo@gmail.com>
2022-01-10 15:44:45 +0900

    chore: divide test files

---
+++ Tests/AceLayoutTests/LayoutTargetTests.swift
@@ -0,0 +1,38 @@
+//
+//  LayoutTargetTests.swift
+//  
+//
+//  Created by Yusaku Nishi on 2022/01/10.
+//
+
+import XCTest
+import AceLayout
+
+final class LayoutTargetTests: XCTestCase {
+    
+    func testAutoLayout() {
+        XCTContext.runActivity(named: "translatesAutoresizingMaskIntoConstraints should become false") { _ in
+            let view = AL.View()
+            XCTAssert(view.translatesAutoresizingMaskIntoConstraints)
+            view.autoLayout { item in
+                item.width.equal(to: 100)
+            }
+            XCTAssertFalse(view.translatesAutoresizingMaskIntoConstraints)
+        }
+        XCTContext.runActivity(named: "isActive control") { _ in
+            let view = AL.View()
+            XCTContext.runActivity(named: "All isActive should become true by default") { _ in
+                let constraints = view.autoLayout { item in
+                    item.size.equal(toSquare: 100)
+                }
+                XCTAssert(constraints.allSatisfy { $0.isActive })
+            }
+            XCTContext.runActivity(named: "All isActive should be false when activates: false") { _ in
+                let constraints = view.autoLayout(activates: false) { item in
+                    item.size.equal(toSquare: 100)
+                }
+                XCTAssert(constraints.allSatisfy { !$0.isActive })
+            }
+        }
+    }
+}
---
+++ Tests/AceLayoutTests/NSLayoutConstraintExtensionTests.swift
@@ -0,0 +1,41 @@
+//
+//  NSLayoutConstraintExtensionTests.swift
+//
+//
+//  Created by Yusaku Nishi on 2021/12/16.
+//
+
+import XCTest
+import AceLayout
+
+final class NSLayoutConstraintExtensionTests: XCTestCase {
+    
+    private var superview: AL.View!
+    private var subview: AL.View!
+    
+    override func setUp() {
+        super.setUp()
+        
+        superview = AL.View()
+        subview = AL.View()
+        superview.addSubview(subview)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        subview.removeFromSuperview()
+        subview = nil
+        superview = nil
+    }
+    
+    func testPriority() {
+        let constraints = subview.autoLayout { item in
+            item.width.equal(to: 100)
+            item.height.equal(to: 100).priority(.defaultLow)
+            item.center.equal(to: superview).priority(.defaultHigh)
+        }
+        XCTAssertEqual(constraints.map { $0.priority },
+                       [.required, .defaultLow, .defaultHigh, .defaultHigh])
+    }
+}
